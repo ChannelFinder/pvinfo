2f822f9793c1dee2aa10a1f2a6acc9078539d944
Mitch Frauenheim <mitch.frauenheim@wustl.edu>
2023-06-20 15:12:22 -0700

    respect disabled checkboxes

--- src/components/home/queryresults/valuecheckbox/ValueCheckbox.js
+++ src/components/home/queryresults/valuecheckbox/ValueCheckbox.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
 import { Checkbox, Tooltip } from '@mui/material';
 import api from '../../../../api';
@@ -23,6 +23,7 @@ function ValueCheckbox(props) {
     // open web socket for sending subscribe/clear messages
     // filter all messages as false since we don't need to read anything in parent component
     // This still is in QueryResults so even with USE_PVWS===false it will try to connect, would be better to move to ValueCheckbox?
+    const [enabled, setEnabled] = useState(true);
     const { sendJsonMessage } = useWebSocket(api.PVWS_URL, {
         share: true,
         filter: (message) => false,
@@ -32,19 +33,27 @@ function ValueCheckbox(props) {
         props.updateCurrentChecked(index, event.target.checked);
     }
 
+    useEffect(() => {
+        if (props.pvStatus === "Inactive" || props.recordType === "waveform") {
+            setEnabled(false);
+        }
+    }, [props.pvStatus, props.recordType])
+
     // watch for changes in checked, if so send a subscribe message
     useEffect(() => {
-        if (props.currentChecked.has(props.id)) {
-            sendJsonMessage({ "type": "subscribe", "pvs": [props.pvName] });
-        } else {
-            sendJsonMessage({ "type": "clear", "pvs": [props.pvName] });
+        if (enabled) {
+            if (props.currentChecked.has(props.id)) {
+                sendJsonMessage({ "type": "subscribe", "pvs": [props.pvName] });
+            } else {
+                sendJsonMessage({ "type": "clear", "pvs": [props.pvName] });
+            }
         }
-    }, [sendJsonMessage, props.currentChecked, props.checked, props.id, props.pvName])
+    }, [sendJsonMessage, props.currentChecked, props.checked, props.id, props.pvName, enabled])
 
     return (
         <Tooltip arrow title={<div>Monitor<br />{props.pvName}</div>}>
             <Checkbox
-                checked={props.checked[props.id]}
+                checked={props.checked[props.id] && enabled}
                 disabled={props.pvStatus === "Inactive" || props.recordType === "waveform"}
                 color="primary"
                 onChange={handleMonitorPVChange(props.id)} >
