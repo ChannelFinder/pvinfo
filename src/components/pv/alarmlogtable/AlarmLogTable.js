f0c7602bfe4e0fb08f585d353183403e732f868f
Mitch Frauenheim <mitch.frauenheim@wustl.edu>
2023-06-26 16:49:07 -0700

    Alarm ui no accordions

--- src/components/pv/alarmlogtable/AlarmLogTable.js
+++ src/components/pv/alarmlogtable/AlarmLogTable.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { Accordion, AccordionDetails, AccordionSummary, Box, Grid, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Chip, Grid, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AlarmVariable from "./alarmvariable/AlarmVariable";
 import api from "../../../api";
@@ -10,12 +10,33 @@ const propTypes = {
 }
 
 const severityColors = {
-    NONE: "green",
+    NONE: "black",
     OK: "green",
     INVALID: "#FF00FF",
     UNDEFINED: "#C800C8",
     MAJOR: "red",
-    MINOR: "#FF9900"
+    MAJOR_ACK: "#8b0000",
+    MINOR: "#FF9900",
+    INVALID_ACK: "#FFFFFF",
+    UNDEFINED_ACK: "#FFFFFF",
+    MINOR_ACK: "#FFFFFF"
+
+}
+
+const severityBackground = {
+    INVALID_ACK: "#FF00FF",
+    UNDEFINED_ACK: "#C800C8",
+    MINOR_ACK: "#FF9900"
+}
+
+const dateFormat = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    fractionalSecondDigits: 3,
 }
 
 function AlarmLogTable(props) {
@@ -76,83 +97,72 @@ function AlarmLogTable(props) {
                             {
                                 alarmLogData.map((item, i) => {
                                     return (
-                                        <Accordion>
-                                            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`"alarm-item-${i}-content"`} id={`alarm-item-${i}-header`}>
+                                        <Box key={i} id={`alarm-item-${i}`} sx={{ display: "flex", flexDirection: "column", py: 1, borderBottom: 2, borderColor: 'grey.300', '&:last-child': { border: 0 } }}>
+                                            <Box id={`alarm-item-${i}-header`} sx={{ display: "flex", flexDirection: "row", alignItems: "center", py: 2, px: 2 }}>
                                                 <Typography sx={{ fontWeight: "medium", fontSize: 14 }}>
                                                     Message Time:
                                                 </Typography>
                                                 <Typography sx={{ fontSize: 14, ml: 1 }}>
                                                     {
                                                         item.message_time ?
-                                                            new Date(item.message_time).toLocaleString("en-US", {
-                                                                year: "numeric",
-                                                                month: "2-digit",
-                                                                day: "2-digit",
-                                                                hour: "2-digit",
-                                                                minute: "2-digit",
-                                                                second: "2-digit",
-                                                                fractionalSecondDigits: 3,
-                                                            })
+                                                            new Date(item.message_time).toLocaleString("en-US", dateFormat)
                                                             : null
                                                     }
                                                 </Typography>
                                                 <Typography sx={{ fontWeight: "medium", fontSize: 14, ml: 3 }}>
                                                     Severity:
                                                 </Typography>
-                                                <Typography sx={{ fontSize: 14, color: item.severity ? severityColors[item.severity] : "black", ml: 1 }}>
-                                                    {item.severity ? item.severity : "None"}
-                                                </Typography>
-                                            </AccordionSummary>
-                                            <AccordionDetails>
-                                                <Box key={i} sx={{ display: "flex", flexDirection: "row", justifyContent: "flex-start", gap: 5, flexWrap: "wrap", p: 1, overflow: "auto" }}>
-                                                    {
-                                                        item.pv ?
-                                                            <AlarmVariable title="PV" value={item.pv} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.severity ?
-                                                            <AlarmVariable title="Severity" value={item.severity} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.message ?
-                                                            <AlarmVariable title="Message" value={item.message} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.value ?
-                                                            <AlarmVariable title="Value" value={item.value} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.time ?
-                                                            <AlarmVariable title="Time" value={item.time} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.config ?
-                                                            <AlarmVariable title="Config" value={item.config} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.user ?
-                                                            <AlarmVariable title="User" value={item.user} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.host ?
-                                                            <AlarmVariable title="Host" value={item.host} />
-                                                            : null
-                                                    }
-                                                    {
-                                                        item.config_msg ?
-                                                            <AlarmVariable title="Config Message" value={item.config_msg} />
-                                                            : null
-                                                    }
-                                                </Box>
-                                            </AccordionDetails>
-                                        </Accordion>
+                                                <Chip label={item.severity ? item.severity : "None"} size="small"
+                                                    sx={{ ml: 1, fontWeight: "medium", fontSize: 12, color: severityColors[item.severity] ? severityColors[item.severity] : "black", backgroundColor: severityBackground[item.severity] ? severityBackground[item.severity] : "#EBEBEB" }}></Chip>
+                                            </Box>
+                                            <Box id={`alarm-item-${i}-body`} sx={{ display: "flex", flexDirection: "row", justifyContent: "flex-start", gap: 5, flexWrap: "wrap", my: 0.5, py: 1, px: 3, overflow: "auto" }}>
+                                                {
+                                                    item.pv ?
+                                                        <AlarmVariable title="PV" value={item.pv} />
+                                                        : null
+                                                }
+                                                {/* {
+                                                    item.severity ?
+                                                        <AlarmVariable title="Severity" value={item.severity} color={severityColors[item.severity] ? severityColors[item.severity] : "black"} />
+                                                        : null
+                                                } */}
+                                                {
+                                                    item.message ?
+                                                        <AlarmVariable title="Message" value={item.message} />
+                                                        : null
+                                                }
+                                                {
+                                                    item.value ?
+                                                        <AlarmVariable title="Value" value={item.value} />
+                                                        : null
+                                                }
+                                                {
+                                                    item.time ?
+                                                        <AlarmVariable title="Time" value={item.time} />
+                                                        : null
+                                                }
+                                                {
+                                                    item.config ?
+                                                        <AlarmVariable title="Config" value={item.config} />
+                                                        : null
+                                                }
+                                                {
+                                                    item.user ?
+                                                        <AlarmVariable title="User" value={item.user} />
+                                                        : null
+                                                }
+                                                {
+                                                    item.host ?
+                                                        <AlarmVariable title="Host" value={item.host} />
+                                                        : null
+                                                }
+                                                {
+                                                    item.config_msg ?
+                                                        <AlarmVariable title="Config Message" value={item.config_msg} />
+                                                        : null
+                                                }
+                                            </Box>
+                                        </Box>
                                     )
                                 })
                             }
--- src/components/pv/alarmlogtable/alarmvariable/AlarmVariable.js
+++ src/components/pv/alarmlogtable/alarmvariable/AlarmVariable.js
@@ -4,13 +4,14 @@ import PropTypes from "prop-types";
 const propTypes = {
     title: PropTypes.string,
     value: PropTypes.string,
+    color: PropTypes.string
 }
 
 function AlarmVariable(props) {
     return (
         <Box sx={{ display: "flex", flexDirection: "column" }}>
             <Typography variant="subtitle2" sx={{ mb: 0.5 }}>{props.title}</Typography>
-            <Typography variant="body2" sx={{ overflow: "hidden" }}>{props.value}</Typography>
+            <Typography variant="body2" sx={{ overflow: "hidden", color: props.color }}>{props.value}</Typography>
         </Box>
     )
 }
